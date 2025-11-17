73ba0dd4c943dc66c261a6808b6963e13fe513b9
rohanuoft <rohan.kuan@mail.utoronto.ca>
2025-11-17 17:09:27 -0500

    refactor: extract getVolumeCredits, use local result - Task 2.2

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -31,13 +31,6 @@ public class StatementPrinter {
 
         for (Performance p : invoice.getPerformances()) {
 
-            // add volume credits
-            volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-            // add extra credit for every five comedy attendees
-            if ("comedy".equals(getPlay(p).getType())) {
-                volumeCredits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
-            }
-
             // print line for this order
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
@@ -45,10 +38,20 @@ public class StatementPrinter {
                     frmt.format(getAmount(p) / Constants.PERCENT_FACTOR),
                     p.getAudience()));
             totalAmount += getAmount(p);
+            volumeCredits += getVolumeCredits(p);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private int getVolumeCredits(Performance performance) {
+        int result = 0;
+        result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        if ("comedy".equals(this.getPlay(performance).getType())) {
+            result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return result;
     }
 
     private Play getPlay(Performance performance) {
