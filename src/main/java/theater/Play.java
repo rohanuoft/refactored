d081acb3a9ded0dfb3a1d464766459eb674619b7
rohanuoft <rohan.kuan@mail.utoronto.ca>
2025-11-16 20:48:58 -0500

    Fixed all CSC207 Checkstyle errors

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,29 @@
 package theater;
 
+/**
+ * Represents a play in the theater billing system.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
+    /**
+     * Constructs a new Play with the given name and type.
+     *
+     * @param name the name of the play, must not be null
+     * @param type the type of the play, must not be null
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
